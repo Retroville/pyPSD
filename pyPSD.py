c91cd9ccf609fb9367d0041c55be8e359736ac98
Retroville <warhodes@gmail.com>
2018-04-02 09:24:59 -0400

    added prompting

--- pyPSD.py
+++ pyPSD.py
@@ -1,42 +1,64 @@
+# Initialization
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 vol_col = []
 ext_col = []
+dat = []
 
+# Data Import & Prompt
 with open('../data/inputex.csv', encoding="latin-1") as csvfile:
 
     csvreader = csv.reader(csvfile)
 
     for row in csvreader:
-        vol_col.append(row[2])
-        ext_col.append(row[3])
+        dat.append(row)
 
+dat = np.array(dat)
+vol_col_strs = 'Select the \'volume\' column: ' + ', '.join(dat[0])
+vol_col_idx = int(input(vol_col_strs)) - 1
+print('You chose: ', dat[0][vol_col_idx])
+
+ext_col_strs = 'Select the data column: ' + ', '.join(dat[0])
+ext_col_idx = int(input(ext_col_strs)) - 1
+print('You chose: ', dat[0][ext_col_idx])
+
+with open('../data/inputex.csv', encoding="latin-1") as csvfile:        # Clean this later
+
+    csvreader = csv.reader(csvfile)
+
+    for row in csvreader:
+        vol_col.append(row[vol_col_idx])
+        ext_col.append(row[ext_col_idx])
+
+
+#my_range = input('Input the range of the x axis bins: ')
+#print('Your range: ' + my_range)
+#my_range = float(my_range)
+
+my_range = 0.005
+
+# Calculations
 volstr = vol_col[0]
 vol_raw = vol_col[1:]
-volume = np.array([float(i) for i in vol_raw])
+volume = np.array(sorted([float(i) for i in vol_raw]))
 volbinsums = []
 
 extstr = ext_col[0]
 ext_raw = ext_col[1:]
-extent = np.array([float(i) for i in ext_raw])
+extent = np.array(sorted([float(i) for i in ext_raw]))
 
-# Enter bin range
-
-my_range = 0.005
 bin_edges = np.arange(0, max(extent), my_range)
-
-# ---
 
 counts = plt.hist(extent, bins=bin_edges)[0]
 
-idx = np.digitize(extent, bin_edges)
+idx = np.searchsorted(bin_edges, extent, 'right')
 for i in range(1, len(bin_edges)):
     logc = idx == i
     volbinsums.append(sum(volume[logc]))
 
+# Plotting
 fig = plt.figure()
 
 ax1 = fig.add_subplot(2, 1, 1)
