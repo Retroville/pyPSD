f05beb1b5ba4651aa9322872b34e90d6df4eebc8
Austin <warhodes@gmail.com>
2018-05-15 11:55:31 -0400

    Complete new version

--- pyPSD.py
+++ pyPSD.py
@@ -1,111 +1,118 @@
-#%% Initialization
-import csv
+# %% Initialization
+import unicodecsv as csv
 import numpy as np
 import matplotlib.pyplot as plt
-import codecs
-import tkinter as tk
-from tkinter import filedialog
+# import tkinter as tk
+# from tkinter import filedialog
 
 vol_col = []
 ext_col = []
 dat = []
 dat_prompt_strs = []
+volbinsums = []
+my_range = []
+# initilization of variables. do I even need to do this? who knows
 
-#%% Data Import & Prompt
-root = tk.Tk()
-root.withdraw()
+# %% Data Import & Prompt
 
-file_path = filedialog.askopenfilename()
+# root = tk.Tk()
+# root.withdraw()
 
-with codecs.open(file_path, 'rb', encoding="latin-1") as csvfile:
-
-    csvreader = csv.reader(csvfile)
+# file_path = filedialog.askopenfilename()
+file_path = '../data/input.csv'  # DEBUGGING - remove this later
+with open(file_path, 'rb') as csvfile:
+    csvreader = csv.reader(csvfile, encoding='utf-8')
 
     for row in csvreader:
+        if 'Grand total' in row:
+            break
         dat.append(row)
-        # Creates list of all columns in file
 
-dat = np.array(dat)
+strs = dat[1]  # pulls column headers
+dat = np.array(dat[2:])  # pulls raw numbers
+strs = strs[1:]  # trims 'name' column
+dat = np.delete(dat, 0, 1)  # trims name data column
+dat = dat.astype(np.float)  # converts raw numbers to float(eg 2.31e7 to float)
 
-for i in range(0, len(dat[0])):
-    dat_prompt_strs.append(str(i+1) + ' - ' + dat[0][i])
+for i in range(0, len(strs)):  # creates prompt string: choice component
+    dat_prompt_strs.append(str(i + 1) + ' - ' + strs[i])
 
 vol_col_strs = 'Select the \'volume\' column: \n' + '\n'.join(dat_prompt_strs)
 vol_col_idx = int(input(vol_col_strs)) - 1
-print('You chose: ', dat[0][vol_col_idx])
+print('You chose: ', strs[vol_col_idx])
 
-ext_col_strs = 'Select the data column:\n' + '\n'.join(dat[0])
+ext_col_strs = 'Select the data column:\n' + '\n'.join(dat_prompt_strs)
 ext_col_idx = int(input(ext_col_strs)) - 1
-print('You chose: ', dat[0][ext_col_idx])
-
-vol_col = dat[1:, vol_col_idx]
-ext_col = dat[1:, ext_col_idx]
+print('You chose: ', strs[ext_col_idx])
 # Imports data based on prompt results
 
 
-my_range = input('Input the range of the x axis bins: ')
+my_range = input('Input the range of the x axis bins:')
 print('Your range: ' + my_range)
-my_range = float(my_range)
 
-my_range = 0.005
+# %% Calculations
+volstr = strs[vol_col_idx]
+volume = dat[:, vol_col_idx]
+extstr = strs[ext_col_idx]
+extent = dat[:, ext_col_idx]  # formats primary data and volume columns
 
-#%% Calculations
-volstr = vol_col[0]
-vol_raw = vol_col[1:]
-volume = np.array(sorted([float(i) for i in vol_raw]))
-volbinsums = []
+if my_range:
+    my_range=float(my_range)
+    bin_edges = np.arange(0, max(extent), my_range)
+else:
+    bin_edges = 'auto'
 
-extstr = ext_col[0]
-ext_raw = ext_col[1:]
-extent = np.array(sorted([float(i) for i in ext_raw]))
+counts, realbins, air = plt.hist(extent, bins=bin_edges)  # create num dist from bin edges [1]
 
-bin_edges = np.arange(0, max(extent), my_range)
+numavg = sum(extent) / len(extent)  # basic average of primary data            [0]
 
-counts = plt.hist(extent, bins=bin_edges)[0]
-
-numavg = sum(extent)/len(extent)
-
-idx = np.searchsorted(bin_edges, extent, 'right')
-for i in range(1, len(bin_edges)):
+idx = np.searchsorted(realbins, extent, 'right')
+for i in range(1, len(realbins)):  # creates binned sums of volumes per count [2]
     logc = idx == i
     volbinsums.append(sum(volume[logc]))
 
-volbinfracsums = volbinsums/sum(volume)
-volfrac = volume/sum(volume)
-volavg = sum(extent*volfrac) / len(extent)
+volbinfracsums = volbinsums / sum(volume)  # basic volume fraction (per bin)   [0]
+volfrac = volume / sum(volume)  # basic volume fraction                        [0]
+volavg = sum(extent * volfrac) / len(extent)  # what even is this?             [3]
 
 binlabels = np.arange(0, len(volbinsums))
 
-#%% Plotting
-plt.figure(1)
-#for i in range(1, len(dat[0])):
-#    plt.subplot(3, 3, i)
-plt.scatter(dat[1:, ext_col_idx], dat[1:, 5])
+# %% Plotting
+plt.clf()
 
-plt.show()
+fig = plt.figure(num=1)
+plt.suptitle(strs[ext_col_idx])
+for i in range(1, len(dat[0])+1):  
+# creates a grid of scatterplots, per each column pair
+    # if i != ext_col_idx+1:  # excludes the 'mirror' scatterplot
+        plt.subplot(2, 2, i)
+        plt.scatter(extent, dat[:, i-1], marker='.', c='black')
+        plt.xlim(0, max(extent))
+        plt.ylim(0, max(dat[:, i-1]))
+        plt.xlabel(strs[i-1])
+fig.set_figheight(8)
+fig.set_figwidth(8)
 
-plt.figure(2)
+plt.figure(num=2, figsize=(8,8))
 plt.tight_layout()
 
 plt.subplot(2, 1, 1)
 plt.bar(binlabels, counts,
-        width=-1, color='green', linewidth=0.2, edgecolor='black', align='edge',
-        tick_label=bin_edges[1:])
+        width=-1, color='green', linewidth=0.2, edgecolor='black',
+        align='edge', tick_label=realbins[1:])
 plt.xlabel(extstr)
 plt.ylabel('Counts')
 plt.xticks(rotation=90)
 
 plt.subplot(2, 1, 2)
 plt.bar(binlabels, volbinsums,
-        width=-1, color='green', linewidth=0.2, edgecolor='black', align='edge',
-        tick_label=bin_edges[1:])
+        width=-1, color='green', linewidth=0.2, edgecolor='black',
+        align='edge', tick_label=realbins[1:])
 plt.xlabel(volstr)
 plt.ylabel('Volume')
-plt.xticks(rotation=90)
+plt.xticks(rotation=90)     # CHECK THAT THE AXIS OF THIS IS JUST WRONG AND NOT THE WHOLE PROGRAM
 
 plt.show()
-
-
 
 print('Number Average: ', numavg)
 print('Volume Average: (Warning) ', volavg)
