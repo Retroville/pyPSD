9dc26de9cc47c85e04e7d72975a979c28556c132
Austin <warhodes@gmail.com>
2018-05-15 15:45:32 -0400

    repaired and reupholstered

--- pyPSD.py
+++ pyPSD.py
@@ -16,11 +16,11 @@ my_range = []
 def voldist(dat, strs):
     #Init:
     volbinsums = []
-    plt.clf()
+
     #----
     my_range = input('Input the range of the x axis bins (Type \'exit:\' to quit):')
     print('Your range: ' + my_range)
-    
+
     volstr = strs[vol_col_idx]
     volume = dat[:, vol_col_idx]
     extstr = strs[ext_col_idx]
@@ -36,24 +36,24 @@ def voldist(dat, strs):
 
     counts, realbins = np.histogram(extent, bins=bin_edges)  # create num dist from bin edges [1]
 
-    numavg = sum(extent) / len(extent)  # basic average of primary data            [0]
+    numavg = sum(extent) / len(extent)  # basic average of primary data        [0]
 
     idx = np.searchsorted(realbins, extent, 'right')
-    
+
     for i in range(1, len(realbins)):  # creates binned sums of volumes per count [2]
         logc = idx == i
         volbinsums.append(sum(volume[logc]))
 
-    #volbinfracsums = volbinsums / sum(volume)  # basic volume fraction (per bin)   [0]
-    volfrac = volume / sum(volume)  # basic volume fraction                        [0]
-    volavg = sum(extent * volfrac) / len(extent)  # what even is this?             [3]
+  # volbinfracsums = volbinsums / sum(volume)  # basic volume fraction (per bin)   [0]
+    volfrac = volume / sum(volume)  # basic volume fraction                    [0]
+    volavg = sum(extent * volfrac) / len(extent)  # what even is this?         [3]
 
     binlabels = np.arange(0, len(volbinsums))
-    
+
     plt.figure(num=2, figsize=(8,8))
     plt.tight_layout()
-
     plt.ion()
+    plt.clf()
 
     plt.subplot(2, 1, 1)
     plt.bar(binlabels, counts,
@@ -70,17 +70,17 @@ def voldist(dat, strs):
     plt.xlabel(volstr)
     plt.ylabel('Volume')
     plt.xticks(rotation=90)
-    plt.show()
+
 
     print('Number Average: ', numavg)
     print('Volume Average: (Warning) ', volavg)
-    
-    return True
 
+    return True
+'''
 def scattergrid(dat, str_col_idx, ext_col_idx):
     fig = plt.figure(num=1)
     plt.suptitle(strs[ext_col_idx])
-    for i in range(1, len(dat[0])+1):  
+    for i in range(1, len(dat[0])+1):
         # creates a grid of scatterplots, per each column pair
         plt.subplot(2, 2, i)
         plt.scatter(dat[:, i-1], dat[:, ext_col_idx], marker='.', c='black')
@@ -90,7 +90,7 @@ def scattergrid(dat, str_col_idx, ext_col_idx):
         plt.ylabel(strs[ext_col_idx])
         fig.set_figheight(8)
         fig.set_figwidth(8)
-    return 
+    return '''
 # %% Data Import & Prompt
 
 # root = tk.Tk()
@@ -125,9 +125,22 @@ print('You chose: ', strs[ext_col_idx])
 # Imports data based on prompt results
 
 # %% Main
-scattergrid(dat, vol_col_idx, ext_col_idx)
+fig = plt.figure(num=1, figsize=(8,8))
+plt.suptitle(strs[ext_col_idx])
+for i in range(1, len(dat[0])+1):
+    # creates a grid of scatterplots, per each column pair
+    plt.subplot(2, 2, i)
+    plt.scatter(dat[:, i-1], dat[:, ext_col_idx], marker='.', c='black', s=1)
+    plt.xlim(0, max(dat[:, i-1]))
+    plt.ylim(0, max(dat[:, ext_col_idx]))
+    plt.xlabel(strs[i-1])
+    plt.ylabel(strs[ext_col_idx])
+fig.set_figheight(8)
+fig.set_figwidth(8)
+
 stay = True
 while stay:
     stay = voldist(dat, strs)
+    plt.show()
 
 
