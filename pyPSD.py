7546886a05b56d551495a60c58d73d8bd2add087
Austin <warhodes@gmail.com>
2018-05-15 14:41:54 -0400

    loopy mode

--- pyPSD.py
+++ pyPSD.py
@@ -9,10 +9,88 @@ vol_col = []
 ext_col = []
 dat = []
 dat_prompt_strs = []
-volbinsums = []
 my_range = []
-# initilization of variables. do I even need to do this? who knows
+#---- initilization of variables. do I even need to do this? who knows
 
+# %% Functions
+def voldist(dat, strs):
+    #Init:
+    volbinsums = []
+    plt.clf()
+    #----
+    my_range = input('Input the range of the x axis bins (Type \'exit:\' to quit):')
+    print('Your range: ' + my_range)
+    
+    volstr = strs[vol_col_idx]
+    volume = dat[:, vol_col_idx]
+    extstr = strs[ext_col_idx]
+    extent = dat[:, ext_col_idx]  # formats primary data and volume columns
+
+    if my_range:
+        my_range=float(my_range)
+        bin_edges = np.arange(0, max(extent), my_range)
+    elif my_range == 'exit':
+        return False
+    else:
+        bin_edges = 'auto'
+
+    counts, realbins = np.histogram(extent, bins=bin_edges)  # create num dist from bin edges [1]
+
+    numavg = sum(extent) / len(extent)  # basic average of primary data            [0]
+
+    idx = np.searchsorted(realbins, extent, 'right')
+    
+    for i in range(1, len(realbins)):  # creates binned sums of volumes per count [2]
+        logc = idx == i
+        volbinsums.append(sum(volume[logc]))
+
+    #volbinfracsums = volbinsums / sum(volume)  # basic volume fraction (per bin)   [0]
+    volfrac = volume / sum(volume)  # basic volume fraction                        [0]
+    volavg = sum(extent * volfrac) / len(extent)  # what even is this?             [3]
+
+    binlabels = np.arange(0, len(volbinsums))
+    
+    plt.figure(num=2, figsize=(8,8))
+    plt.tight_layout()
+
+    plt.ion()
+
+    plt.subplot(2, 1, 1)
+    plt.bar(binlabels, counts,
+            width=-1, color='green', linewidth=0.2, edgecolor='black',
+            align='edge', tick_label=realbins[1:])
+    plt.xlabel(extstr)
+    plt.ylabel('Counts')
+    plt.xticks(rotation=90)
+
+    plt.subplot(2, 1, 2)
+    plt.bar(binlabels, volbinsums,
+            width=-1, color='green', linewidth=0.2, edgecolor='black',
+            align='edge', tick_label=realbins[1:])
+    plt.xlabel(volstr)
+    plt.ylabel('Volume')
+    plt.xticks(rotation=90)
+    plt.show()
+
+    print('Number Average: ', numavg)
+    print('Volume Average: (Warning) ', volavg)
+    
+    return True
+
+def scattergrid(dat, str_col_idx, ext_col_idx):
+    fig = plt.figure(num=1)
+    plt.suptitle(strs[ext_col_idx])
+    for i in range(1, len(dat[0])+1):  
+        # creates a grid of scatterplots, per each column pair
+        plt.subplot(2, 2, i)
+        plt.scatter(dat[:, i-1], dat[:, ext_col_idx], marker='.', c='black')
+        plt.xlim(0, max(dat[:, i-1]))
+        plt.ylim(0, max(dat[:, ext_col_idx]))
+        plt.xlabel(strs[i-1])
+        plt.ylabel(strs[ext_col_idx])
+        fig.set_figheight(8)
+        fig.set_figwidth(8)
+    return 
 # %% Data Import & Prompt
 
 # root = tk.Tk()
@@ -46,73 +124,10 @@ ext_col_idx = int(input(ext_col_strs)) - 1
 print('You chose: ', strs[ext_col_idx])
 # Imports data based on prompt results
 
+# %% Main
+scattergrid(dat, vol_col_idx, ext_col_idx)
+stay = True
+while stay:
+    stay = voldist(dat, strs)
 
-my_range = input('Input the range of the x axis bins:')
-print('Your range: ' + my_range)
 
-# %% Calculations
-volstr = strs[vol_col_idx]
-volume = dat[:, vol_col_idx]
-extstr = strs[ext_col_idx]
-extent = dat[:, ext_col_idx]  # formats primary data and volume columns
-
-if my_range:
-    my_range=float(my_range)
-    bin_edges = np.arange(0, max(extent), my_range)
-else:
-    bin_edges = 'auto'
-
-counts, realbins, air = plt.hist(extent, bins=bin_edges)  # create num dist from bin edges [1]
-
-numavg = sum(extent) / len(extent)  # basic average of primary data            [0]
-
-idx = np.searchsorted(realbins, extent, 'right')
-for i in range(1, len(realbins)):  # creates binned sums of volumes per count [2]
-    logc = idx == i
-    volbinsums.append(sum(volume[logc]))
-
-volbinfracsums = volbinsums / sum(volume)  # basic volume fraction (per bin)   [0]
-volfrac = volume / sum(volume)  # basic volume fraction                        [0]
-volavg = sum(extent * volfrac) / len(extent)  # what even is this?             [3]
-
-binlabels = np.arange(0, len(volbinsums))
-
-# %% Plotting
-plt.clf()
-
-fig = plt.figure(num=1)
-plt.suptitle(strs[ext_col_idx])
-for i in range(1, len(dat[0])+1):  
-# creates a grid of scatterplots, per each column pair
-    # if i != ext_col_idx+1:  # excludes the 'mirror' scatterplot
-        plt.subplot(2, 2, i)
-        plt.scatter(extent, dat[:, i-1], marker='.', c='black')
-        plt.xlim(0, max(extent))
-        plt.ylim(0, max(dat[:, i-1]))
-        plt.xlabel(strs[i-1])
-fig.set_figheight(8)
-fig.set_figwidth(8)
-
-plt.figure(num=2, figsize=(8,8))
-plt.tight_layout()
-
-plt.subplot(2, 1, 1)
-plt.bar(binlabels, counts,
-        width=-1, color='green', linewidth=0.2, edgecolor='black',
-        align='edge', tick_label=realbins[1:])
-plt.xlabel(extstr)
-plt.ylabel('Counts')
-plt.xticks(rotation=90)
-
-plt.subplot(2, 1, 2)
-plt.bar(binlabels, volbinsums,
-        width=-1, color='green', linewidth=0.2, edgecolor='black',
-        align='edge', tick_label=realbins[1:])
-plt.xlabel(volstr)
-plt.ylabel('Volume')
-plt.xticks(rotation=90)     # CHECK THAT THE AXIS OF THIS IS JUST WRONG AND NOT THE WHOLE PROGRAM
-
-plt.show()
-
-print('Number Average: ', numavg)
-print('Volume Average: (Warning) ', volavg)
