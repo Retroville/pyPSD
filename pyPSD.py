91b7ad37d0425c09884237a7d97264de322876f4
Austin <warhodes@gmail.com>
2018-06-12 15:36:37 -0400

    reportmode working, premulticolumn

--- pyPSD.py
+++ pyPSD.py
@@ -6,287 +6,306 @@ import os
 import errno
 import matplotlib.pyplot as plt     
 import sys
+from pyfiglet import figlet_format
 # import tkinter as tk
 # from tkinter import filedialog
 
 try:
-    os.makedirs('../output/')
+	os.makedirs('../output/')
 except OSError as e:
-    if e.errno != errno.EEXIST:
-        raise
+	if e.errno != errno.EEXIST:
+		raise
 
 # %% Calculations
 class voldist(object):
-    def __init__(self, dat, strs, bin_edges, col_idces, *typ):
-        self.volbinsums = []
-        self.ext_col_idx = col_idces[1]
-        self.vol_col_idx = col_idces[0]
+	def __init__(self, dat, strs, bin_edges, col_idces, *typ):
+		self.volbinsums = []
+		self.ext_col_idx = col_idces[1]
+		self.vol_col_idx = col_idces[0]
 
-        self.volstr = strs[self.vol_col_idx]
-        self.volume = dat[:, self.vol_col_idx]
-        self.extstr = strs[self.ext_col_idx]
-        self.extent = dat[:, self.ext_col_idx]  # formats primary data and volume columns
+		self.volstr = strs[self.vol_col_idx]
+		self.volume = dat[:, self.vol_col_idx]
+		self.extstr = strs[self.ext_col_idx]
+		self.extent = dat[:, self.ext_col_idx]  # formats primary data and volume columns
 
-        self.counts, self.realbins = np.histogram(self.extent, bins=bin_edges) 
-        plt.show
+		self.counts, self.realbins = np.histogram(self.extent, bins=bin_edges) 
+		plt.show
 
-        self.numavg = sum(self.extent) / len(self.extent) 
+		self.numavg = sum(self.extent) / len(self.extent) 
 
-        fidx = np.searchsorted(self.realbins, self.extent, 'right')
+		fidx = np.searchsorted(self.realbins, self.extent, 'right')
 
-        for i in range(1, len(self.realbins)): 
-            logc = fidx == i
-            self.volbinsums.append(sum(self.volume[logc]))
+		for i in range(1, len(self.realbins)): 
+			logc = fidx == i
+			self.volbinsums.append(sum(self.volume[logc]))
 
-      # volbinfracsums = volbinsums / sum(volume)
-        self.volfrac = self.volume / sum(self.volume)  
-        if typ not in locals() or typ == (0,0):
-            self.volavg = sum(self.extent * self.volfrac) / sum(self.volfrac)  
-        else:
-            self.volavg = sum(self.extent**typ[0])/sum(extent**typ[1])
-        
-        self.navgstr = 'Number average: ' + str(float('%.8f'%(self.numavg)))
-        self.vavgstr = 'Volume average: ' + str(float('%.8f'%(self.volavg)))
+	  # volbinfracsums = volbinsums / sum(volume)
+		self.volfrac = self.volume / sum(self.volume)  
+		if typ not in locals() or typ == (0,0):
+			self.volavg = sum(self.extent * self.volfrac) / sum(self.volfrac)  
+		else:
+			self.volavg = sum(self.extent**typ[0])/sum(extent**typ[1])
+		
+		self.navgstr = 'Number average: ' + str(float('%.8f'%(self.numavg)))
+		self.vavgstr = 'Volume average: ' + str(float('%.8f'%(self.volavg)))
 
-        self.binlabels = range(0, len(self.volbinsums))
-        
-        self.current_file_name = "".join([x if x.isalnum() else "_" for x in self.extstr])
-        return
+		self.binlabels = range(0, len(self.volbinsums))
+		
+		self.current_file_name = "".join([x if x.isalnum() else "_" for x in self.extstr])
+		return
 
 # %% Plotting and output
-    def vdplot(self):
+	def vdplot(self):
+		plt.figure(num=2, figsize=(8, 8))
+		plt.ion()
+		plt.clf()
+		def subhistplots(num, xvals, yvals, xstr, ystr):
+			plt.subplot(2, 1, num)
+			plt.bar(xvals, yvals,
+					width=-1, color='white', linewidth=1, edgecolor='red',
+					hatch='////', align='edge', tick_label=np.around(self.realbins[1:],5))
+			plt.xlabel(ystr)
+			plt.ylabel(xstr)
+			plt.xticks(rotation=90)
+			return
+		subhistplots(1, self.binlabels, self.counts, 'Counts', self.extstr)
+		subhistplots(2, self.binlabels, self.volbinsums, 'Volume', self.extstr)
+		plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+		plt.annotate(self.navgstr, xy=(0.55, 0.9), xytext=(0.55, 0.9), 
+						 textcoords='axes fraction')
+		plt.annotate(self.vavgstr, xy=(0.55, 0.8), xytext=(0.55, 0.8),
+						 textcoords='axes fraction') 
+		return
 
-        plt.figure(num=2, figsize=(8, 8))
-        plt.ion()
-        plt.clf()
-
-        def subhistplots(num, xvals, yvals, xstr, ystr):
-            plt.subplot(2, 1, num)
-            plt.bar(xvals, yvals,
-                    width=--1, color='white', linewidth=1, edgecolor='red',
-                    hatch='////', align='edge', tick_label=np.around(self.realbins[1:],5))
-            plt.xlabel(ystr)
-            plt.ylabel(xstr)
-            plt.xticks(rotation=90)
-            plt.annotate(self.navgstr, xy=(0.55, 0.9), xytext=(0.55, 0.9), 
-                         textcoords='axes fraction')
-            plt.annotate(self.vavgstr, xy=(0.55, 0.8), xytext=(0.55, 0.8),
-                         textcoords='axes fraction')
-            return
-        subhistplots(1, self.binlabels, self.counts, 'Counts', self.extstr)
-        subhistplots(2, self.binlabels, self.volbinsums, 'Volume', self.extstr)
-        plt.tight_layout(rect=[0, 0.03, 1, 0.95])
-        return
-
-    def writeout(self):
-        binout = self.realbins[1:]
-        ostr = [self.extstr, 'Counts', 'Volume']
-        odat = np.column_stack([binout, self.counts, self.volbinsums])
-        out = np.vstack([ostr,odat])
-        with open('../output/' + self.current_file_name + '_output.csv', 'wb') as csvout:
-            outputwriter = csv.writer(csvout, delimiter=',')
-            outputwriter.writerows(out)
-            print(color("Plot saved as output.csv", 'green') + '\n')
-        return
-    
-    def saveout(self):
-        plt.figure(1)
-        plt.savefig('../output/' + self.current_file_name + '_scatter')
-        plt.figure(2)
-        plt.savefig('../output/' + self.current_file_name + '_distribution')
-        print(color("Plots saved as ", 'green') + self.current_file_name + 
-                    '_scatter.png' + color(" and ", 'green') + 
-                    self.current_file_name + '_distribution.png' + 
-                    color(" in ", 'green') + "../output/ " + 
-                    color("directory\n", 'green'))
-        
+	def writeout(self):
+		binout = self.realbins[1:]
+		ostr = [self.extstr, 'Counts', 'Volume']
+		odat = np.column_stack([binout, self.counts, self.volbinsums])
+		out = np.vstack([ostr,odat])
+		with open('../output/' + self.current_file_name + '_output.csv', 'wb') as csvout:
+			outputwriter = csv.writer(csvout, delimiter=',')
+			outputwriter.writerows(out)
+			print(color("Plot saved as output.csv", 'green') + '\n')
+		return
+	
+	def saveout(self):
+		plt.figure(1)
+		plt.savefig('../output/' + self.current_file_name + '_scatter')
+		plt.figure(2)
+		plt.savefig('../output/' + self.current_file_name + '_distribution')
+		print(color("Plots saved as ", 'green') + self.current_file_name + 
+					'_scatter.png' + color(" and ", 'green') + 
+					self.current_file_name + '_distribution.png' + 
+					color(" in ", 'green') + "../output/ " + 
+					color("directory\n", 'green'))
+		
 def scattergrid(dat, strs, vol_col_idx, ext_col_idx):
-    gridsize = 1 + len(dat[0])//2
-    scatterplots = plt.figure(num=1, figsize=(8, 8))
-    plt.suptitle(strs[ext_col_idx])
-    for i in range(1, len(dat[0])+1):
-        # creates a grid of scatterplots, per each column pair
-        plt.subplot(gridsize, 2, i)
-        if not i-1 == ext_col_idx:
-            plt.scatter(dat[:, i-1], dat[:, ext_col_idx], marker='.', c='black', s=1)
-            plt.xlim(0, max(dat[:, i-1]))
-          # plt.ylim(0, max(dat[:, ext_col_idx-1]))
-        else:
-            plt.plot([0, 0, 1, 1, 0, 1, 1, 0],[0, 1, 0, 1, 0, 0, 1, 1],'r')
-            plt.xticks([])
-            plt.yticks([])
-        plt.xlabel(strs[i-1])
-    scatterplots.set_figheight(8)
-    scatterplots.set_figwidth(8)
-    plt.gcf().tight_layout() # rect=[0, 0.03, 1, 0.95]
-    plt.subplots_adjust(top=0.95, left=0.1, right = 0.9)
-    return
+	gridsize = 1 + len(dat[0])//2
+	scatterplots = plt.figure(num=1, figsize=(8, 8))
+	plt.suptitle(strs[ext_col_idx])
+	for i in range(1, len(dat[0])+1):
+		# creates a grid of scatterplots, per each column pair
+		plt.subplot(gridsize, 2, i)
+		if not i-1 == ext_col_idx:
+			plt.scatter(dat[:, i-1], dat[:, ext_col_idx], marker='.', c='black', s=1)
+			plt.xlim(0, max(dat[:, i-1]))
+		  # plt.ylim(0, max(dat[:, ext_col_idx-1]))
+		else:
+			plt.plot([0, 0, 1, 1, 0, 1, 1, 0],[0, 1, 0, 1, 0, 0, 1, 1],'r')
+			plt.xticks([])
+			plt.yticks([])
+		plt.xlabel(strs[i-1])
+
+	scatterplots.set_figheight(8)
+	scatterplots.set_figwidth(8)
+	plt.gcf().tight_layout() # rect=[0, 0.03, 1, 0.95]
+	plt.subplots_adjust(top=0.95, left=0.1, right = 0.9)
+	return
 
 def clearplots():
-    plt.figure(1)
-    plt.clf()
-    plt.figure(2)
-    plt.clf()
+	plt.figure(1)
+	plt.clf()
+	plt.figure(2)
+	plt.clf()
 
 # %% Menu                        FIX LATER: i.e. become if: elif: chain :(
 def cmd_save(v):
-    return v.saveout()
+	return v.saveout()
 def cmd_csv(v):
-    return v.writeout()
-def cmd_next(v):
-    clearplots()
-    global idx
-    global sig
-    idx += 1
-    sig = False
-    return
-def cmd_quit(v):
-    sys.exit()
-def cmd_help(v):
-    return
+	return v.writeout()
+def cmd_next():
+	clearplots()
+	global idx
+	global sig
+	idx += 1
+	sig = False
+	return
+def cmd_quit():
+	sys.exit()
+def cmd_help():
+	print(figlet_format('pyPSD', font='big'))
+	print('Python Pore Size Analyzer')
+	print('Austin Rhodes (C) 2018\n')
+	egg = input()
+	if egg == 'about':
+		print('This software fulfulls the specific needs of Dr. Choo\'s research group at ' +
+			  'The University of Tennessee, Knoxville. The primary function is to take in ' +
+			  'pore/particle size data from ScanIP (csv) and generate plots/reports to quickly ' +
+			  'gauge correlations and general usefulness of the data. Originally developed ' +
+			  'in MATLAB as a ' +
+			  'way to plot basic pore size distribution histograms, the project was later ' + 
+			  'recreated in python and heavily expanded upon to add features such as ' +
+			  'scatterplots, outputting to image/csv, interactive plotting, a pdf report mode, ' +
+			  'and eventually inputs of multiple scanIP files.')
+	else:
+		return
 
 def menu_cmd(v):
-    OPTIONS = {"bins":dict( desc = "Change bins of currently active distribution plot", func = None), # wew lad
-               "save":dict( desc = "Save currently active plots as images", func = cmd_save),
-               "csv":dict( desc = "Export currently active plots to csv file", func = cmd_csv),
-               "next":dict( desc = "Select next data column (retains volume column selection)", func = cmd_next),
-               "quit":dict( desc = "Exits the program", func = cmd_quit),
-               "help":dict( desc = "Displays helpful information", func = cmd_help)}
-    while sig == True:
-        print("\nPlease choose an option:")
-        for key in OPTIONS.keys():
-            print("\t" + key + "\t" + OPTIONS[key]["desc"])
-        cmd = input('Selection: ')
-        if not cmd in OPTIONS.keys():
-            print(color("Invalid selection", 'red') + '\n')
-        elif cmd == 'bins':
-            return
-        else:
-            OPTIONS[cmd]["func"](v)
-    return
+	OPTIONS = {"bins":dict( desc = "Change bins of currently active distribution plot", func = None), # wew lad
+			   "save":dict( desc = "Save currently active plots as images", func = cmd_save),
+			   "csv":dict( desc = "Export currently active plots to csv file", func = cmd_csv),
+			   "next":dict( desc = "Select next data column (retains volume column selection)", func = cmd_next),
+			   "about":dict( desc = "Displays helpful information", func = cmd_help),
+			   "quit":dict( desc = "Exits the program", func = cmd_quit)}
+
+	while sig == True:
+		print("\nPlease choose an option:")
+		for key in OPTIONS.keys():
+			print("\t" + key + "\t" + OPTIONS[key]["desc"])
+		cmd = input('Selection: ')
+		if not cmd in OPTIONS.keys():
+			print(color("Invalid selection", 'red') + '\n')
+		elif cmd == 'bins':
+			return
+		elif (cmd == 'save') or (cmd == 'csv'):
+			OPTIONS[cmd]["func"](v)
+		else:
+			OPTIONS[cmd]["func"]()
+	return
 
 # %% Data Import & Prompt
 def get_data():
-    dat = []
-    dat_prompt_strs = []
+	dat = []
+	dat_prompt_strs = []
 
-    # root = tk.Tk()
-    # root.withdraw()
+	# root = tk.Tk()
+	# root.withdraw()
 
-    # file_path = filedialog.askopenfilename()
-    file_path = '../data/input.csv'  # DEBUGGING - remove this later
-    with open(file_path, 'rb') as csvfile:
-        csvreader = csv.reader(csvfile, encoding='utf-8')
+	# file_path = filedialog.askopenfilename()
+	file_path = '../data/input.csv'  # DEBUGGING - remove this later
+	with open(file_path, 'rb') as csvfile:
+		csvreader = csv.reader(csvfile, encoding='utf-8')
 
-        for row in csvreader:
-            if 'Grand total' in row:
-                break
-            dat.append(row)
+		for row in csvreader:
+			if 'Grand total' in row:
+				break
+			dat.append(row)
 
-    strs = dat[1]  # pulls column headers
-    dat = np.array(dat[2:])  # pulls raw numbers
-    strs = strs[1:]  # trims 'name' column
-    dat = np.delete(dat, 0, 1)  # trims name data column
-    dat = dat.astype(np.float)  # converts raw numbers to float(eg 2.31e7 to float)
+	strs = dat[1]  # pulls column headers
+	dat = np.array(dat[2:])  # pulls raw numbers
+	strs = strs[1:]  # trims 'name' column
+	dat = np.delete(dat, 0, 1)  # trims name data column
+	dat = dat.astype(np.float)  # converts raw numbers to float(eg 2.31e7 to float)
 
-    for i in range(0, len(strs)):  # creates prompt string: choice component
-        dat_prompt_strs.append(str(i + 1) + ' - ' + strs[i])
-    return dat, strs, dat_prompt_strs
+	for i in range(0, len(strs)):  # creates prompt string: choice component
+		dat_prompt_strs.append(str(i + 1) + ' - ' + strs[i])
+	return dat, strs, dat_prompt_strs
 
 def get_bins(dat, ext_col_idx):
-    while True:
-        my_range = input('Input the range of the x axis histogram bins: \n'
-                 '(Leave blank to bin automatically)')
-        print(color('You Chose: ', 'green') + my_range + '\n')
-        try:
-            if my_range:
-                my_range = float(my_range)
-                bin_edges = np.arange(0, max(dat[:, ext_col_idx]), my_range)
-            else:
-                bin_edges = 20
-        except ValueError:
-            print(color("Input must be number or null (empty)!\n",'red'))
-        else:
-            break
-    return bin_edges
+	while True:
+		my_range = input('Input the range of the x axis histogram bins: \n'
+				 '(Leave blank to bin automatically)')
+		print(color('You Chose: ', 'green') + my_range + '\n')
+		try:
+			if my_range:
+				my_range = float(my_range)
+				bin_edges = np.arange(0, max(dat[:, ext_col_idx]), my_range)
+			else:
+				bin_edges = 20
+		except ValueError:
+			print(color("Input must be number or null (empty)!\n",'red'))
+		else:
+			break
+	return bin_edges
 
 
 def get_datcol(strs, pstrs):
-    ext_col_strs = 'Select the data column(s):\n' + '\n'.join(pstrs)
-    while True:
-        ext_col_idx = input(ext_col_strs)
-        try:    
-            if ',' in ext_col_idx:
-                ext_col_idx = [int(i)-1 for i in ext_col_idx.split(',')]
-                print(color('You chose: ', 'green') + 'Multiple input columns' + '\n')
-            elif ':' in ext_col_idx:
-                minmax = [int(i)-1 for i in ext_col_idx.split(':')]
-                ext_col_idx = range(minmax[0],minmax[1]+1)
-                print(color('You chose: ', 'green') + 'Range of input columns' + '\n')
-            else:
-                ext_col_idx = int(ext_col_idx)-1
-                print(color('You chose: ', 'green') + strs[ext_col_idx] + '\n')
-        except ValueError:
-            print(color("Input must be integer!\n",'red'))
-        else:
-            break
-    return ext_col_idx
+	ext_col_strs = 'Select the data column(s):\n' + '\n'.join(pstrs)
+	while True:
+		ext_col_idx = input(ext_col_strs)
+		try:    
+			if ',' in ext_col_idx:
+				ext_col_idx = [int(i)-1 for i in ext_col_idx.split(',')]
+				print(color('You chose: ', 'green') + 'Multiple input columns' + '\n')
+			elif ':' in ext_col_idx:
+				minmax = [int(i)-1 for i in ext_col_idx.split(':')]
+				ext_col_idx = range(minmax[0],minmax[1]+1)
+				print(color('You chose: ', 'green') + 'Range of input columns' + '\n')
+			else:
+				ext_col_idx = int(ext_col_idx)-1
+				print(color('You chose: ', 'green') + strs[ext_col_idx] + '\n')
+		except ValueError:
+			print(color("Input must be integer!\n",'red'))
+		else:
+			break
+	return ext_col_idx
 
 def get_volcol(strs, pstrs):
-    vol_col_strs = 'Select the \'volume\' column: \n' + '\n'.join(pstrs)
-    while True: 
-        vol_col_idx = input(vol_col_strs)
-        try:
-            vol_col_idx = int(vol_col_idx) - 1
-        except ValueError:
-            print(color("Input must be integer!\n",'red'))
-        else:
-            break
-    print(color('You chose: ', 'green') + strs[vol_col_idx] + '\n')   
-    return vol_col_idx
+	vol_col_strs = 'Select the \'volume\' column: \n' + '\n'.join(pstrs)
+	while True: 
+		vol_col_idx = input(vol_col_strs)
+		try:
+			vol_col_idx = int(vol_col_idx) - 1
+		except ValueError:
+			print(color("Input must be integer!\n",'red'))
+		else:
+			break
+	print(color('You chose: ', 'green') + strs[vol_col_idx] + '\n')   
+	return vol_col_idx
 
 # %% Main Loop
 def main():
-    global idx
-    global sig
-    global endx
-    idx = 0
-    typ = (4,3) 
-        # Specifiy method of determining volume average:
-        # (Leave blank to calculate weighted average)
-        # Fill in according to D[x,y] parameter
-        # e.g.: De Brouckere mean dia. = (4,3),  Sauter mean dia. = (3,2)
-    dat, strs, dat_prompt_strs = get_data()
-    vol_col_idx = get_volcol(strs, dat_prompt_strs)
-    ext_col_ = get_datcol(strs, dat_prompt_strs)
+	global idx
+	global sig
+	global endx
+	idx = 0
+	typ = (0,0) 
+		# Specifiy method of determining volume average:
+		# (Leave blank to calculate weighted average)
+		# Fill in according to D[x,y] parameter
+		# e.g.: De Brouckere mean dia. = (4,3),  Sauter mean dia. = (3,2)
+	dat, strs, dat_prompt_strs = get_data()
+	vol_col_idx = get_volcol(strs, dat_prompt_strs)
+	ext_col_ = get_datcol(strs, dat_prompt_strs)
 
-    while True:
-        if type(ext_col_) is int:
-            if idx != 0:
-                print(color('End of file reached\n', 'blue') + 
-                      color('Closing Program . . .', 'red'))
-                break
-            ext_col_idx = ext_col_
+	while True:
+		if type(ext_col_) is int:
+			if idx != 0:
+				print(color('End of file reached\n', 'blue') + 
+					  color('Closing Program . . .', 'red'))
+				break
+			ext_col_idx = ext_col_
 
-        else:
-            try:
-                ext_col_idx = ext_col_[idx]
-            except IndexError:
-                print(color('End of file reached\n', 'blue') + 
-                      color('Closing Program . . .', 'red'))
-                break
-        fig1 = scattergrid(dat, strs, vol_col_idx, ext_col_idx)
-        plt.show(block=False)
-        sig = True
-        while sig == True:
-            v = voldist(dat, strs, get_bins(dat, ext_col_idx), [vol_col_idx, ext_col_idx])
-            fig2 = v.vdplot()
-            plt.show()
-            menu_cmd(v)
-    return
+		else:
+			try:
+				ext_col_idx = ext_col_[idx]
+			except IndexError:
+				print(color('End of file reached\n', 'blue') + 
+					  color('Closing Program . . .', 'red'))
+				break
+		fig1 = scattergrid(dat, strs, vol_col_idx, ext_col_idx)
+		plt.show(block=False)
+		sig = True
+		while sig == True:
+			v = voldist(dat, strs, get_bins(dat, ext_col_idx), 
+						[vol_col_idx, ext_col_idx])
+			fig2 = v.vdplot()
+			plt.show()
+			menu_cmd(v)
+	return
 
 if __name__ == "__main__":
-    main()
+	main()
 
 # END, FIN, QED, ETC
 
--- pyPSD_report.py
+++ pyPSD_report.py
@@ -8,94 +8,135 @@ from pylab import *
 from termcolor import colored as clr
 
 try:
-    os.makedirs('../output/')
+	os.makedirs('../output/')
 except OSError as e:
-    if e.errno != errno.EEXIST:
-        raise
+	if e.errno != errno.EEXIST:
+		raise
 
 # %% Gather Data
-idx = 0
-
 dat, strs, dat_prompt_strs = get_data()
 vol_col_idx = get_volcol(strs, dat_prompt_strs)
 ext_col_ = get_datcol(strs, dat_prompt_strs)
-if type(ext_col_) is int:
-	ext_col_idx = ext_col_
-else:
-	ext_col_idx = ext_col_[idx]
 
-typ = (0,0)
-v = voldist(dat, strs, 25, [ext_col_idx, vol_col_idx], typ)
+#enter filename for output:
+filename = 'testoutputTRU'
 
-
-'''
-pdf = PdfPages('../output/testoutput.pdf')
-
-scatterplots = plt.figure(num=1, figsize=(8.5, 11))
-plt.suptitle(strs[ext_col_idx])
-for i in range(0, len(dat[0])):
-	plt.subplot(4, 2, (i%8)+1)
-	if not i == ext_col_idx:
-		plt.scatter(dat[:, (i%8)], dat[:, ext_col_idx], marker='.', c='black', s=2)
-		plt.xlim(0, max(dat[:, (i%8)]))
-	  # plt.ylim(0, max(dat[:, ext_col_idx-1]))
-	else:
-		plt.plot([0, 0, 1, 1, 0, 1, 1, 0],[0, 1, 0, 1, 0, 0, 1, 1],'r')
-		plt.xticks([])
-		plt.yticks([])
-	if (i%8 == 0) and (i != 0):
-		pdf.savefig(scatterplots)
-		scatterplots = plt.figure(num=1, figsize=(8.5, 11))
-		plt.suptitle(strs[ext_col_idx])
-
-
-'''
 numofpages = len(dat[0])//8
 if not len(dat[0])%8 == 0:
 	numofpages += 1
-pages = [plt.figure(num=n+1, figsize=(8.5, 11)) for n in range(len(dat[0])//8)]
 
-print('total colimns: ' + clr(str(len(dat[0])), 'magenta'))
-print('expected pages: ' + clr(numofpages, 'magenta'))
-print('current active figures: ' + clr(len(pages),'magenta'))
-print('figure pointers: ' + clr(pages,'magenta'))
+print('creating empty pdf file at ' + '../output/' + filename + '.pdf:\n\t')
+pages = [plt.figure(num=n+1, figsize=(8.5, 11)) for n in range(numofpages)]
+print(str(numofpages) + ' empty pages\n\t' + str(len(pages)) + 
+		' active empty figures')
+print(pages)
+
+print('creating ' + str(len(dat[0])) + ' empty scatterplots')
+# void
+print('done: \n\t' + str(len(dat[0])) + ' empty scatterplots')
 
 print('\nbeginning page creation loop...\n')
-with PdfPages('../output/testpdfreg.pdf') as pdf:
-	for i in range(0, len(dat[0])):
-		# creates a grid of scatterplots, per each column pair
-		
-		plt.subplot(4, 2, (i%8)+1)
-		if not i == ext_col_idx:
-			plt.scatter(dat[:, i], dat[:, ext_col_idx], marker='.', c='black', s=2)
-			plt.xlim(0, max(dat[:, i]))
-		  # plt.ylim(0, max(dat[:, ext_col_idx-1]))
+print('setting current page to 1...')
+currentpage = 1
+with PdfPages('../output/' + filename + '.pdf') as pdf:
+	#for idx in range(0, nreports)
+	if True:
+		if type(ext_col_) is int:
+			ext_col_idx = ext_col_
+			nreports = 1
 		else:
-			plt.plot([0, 0, 1, 1, 0, 1, 1, 0],[0, 1, 0, 1, 0, 0, 1, 1],'r')
-			plt.xticks([])
-			plt.yticks([])
-		plt.xlabel(strs[i])
+			ext_col_idx = ext_col_[idx]
+			nreports = len(ext_col_)
 
-		print('Figure ' + clr(i, 'cyan') + ', ' + clr(strs[i],'magenta') +
-			  'in position ' + clr((i%8)+1, 'magenta') + 'on page ' + 
-			  clr((i//8)+1, 'magenta'))
-		test = (i%8)+1
-		testcase = test == 0
-		print(str(test) + str(testcase))
-		if ((i%8) == 0) and (i != 0):
-			print('formatting plot...')
-			scatterplots.set_figheight(11)
-			scatterplots.set_figwidth(8.5)
-			plt.suptitle(strs[ext_col_idx])
-			plt.gcf().tight_layout() # rect=[0, 0.03, 1, 0.95]
-			plt.subplots_adjust(top=0.95, left=0.1, right = 0.9)
+		typ = (0,0)
+		v = voldist(dat, strs, 25, [ext_col_idx, vol_col_idx], typ)
 
-			print('\nsaving page...')
-			pdf.savefig()
-			print('page saving complete!')
-			print('closing active page...')
-			plt.close()
-			print('active page closed!')
+		for i in range(0, len(dat[0])):
+			# creates a grid of scatterplots, per each column pair
+			plt.figure(currentpage)
+			plt.subplot(4, 2, (i%8)+1)
+
+			print('creating scatterplot ' + str(i+1) + ' of ' + str(len(dat[0])))
+			print('plotting ' + strs[i] + ' [pos' + str((i%8)+1) + ' pg' + str((i//8)+1) + ']...', end="")
+
+			if not i == ext_col_idx:
+				plt.scatter(dat[:, i], dat[:, ext_col_idx], marker='.', c='black', s=2)
+				plt.xlim(0, max(dat[:, i]))
+			  # plt.ylim(0, max(dat[:, ext_col_idx-1]))
+			else:
+				plt.plot([0, 0, 1, 1, 0, 1, 1, 0],[0, 1, 0, 1, 0, 0, 1, 1],'r')
+				plt.xticks([])
+				plt.yticks([])
+			plt.xlabel(strs[i])
+
+			print('done')
+
+			if ((((i+1)%8) == 0) and (i != 0)) or (i+1 == len(dat[0])):
+				print('formatting plot' + '...', end="")
+				plt.figure(currentpage).set_figheight(11)
+				plt.figure(currentpage).set_figwidth(8.5)
+				plt.suptitle(strs[ext_col_idx])
+				plt.gcf().tight_layout() # rect=[0, 0.03, 1, 0.95]
+				plt.subplots_adjust(top=0.95, left=0.1, right = 0.9)
+				print('done')
+
+				print('saving page' + '...', end="")
+				pdf.savefig()
+				print('done')
+				print('closing active page' + '...', end="")
+				plt.close()
+				print('done')
+				if not i+1 == len(dat[0]):
+					print('incrementing currentpage from ' + str(currentpage) + 
+						  ' to ' + str(currentpage+1) + '...')
+					currentpage += 1
+					print('beginning page creation loop...')
+				else:
+					print('no more plots, closing...')
+					print(color('scatterplot pages complete, ' +
+								'switching to histogram mode', 'green'))
+
+
+		#Distribution plots
+		print('performing volume distribution calculations' + '...', end="")
+		v = voldist(dat, strs, 25, [vol_col_idx, ext_col_idx])
+		print('done')
+		print('formatting histogram page' + '...', end="")
+		plt.figure(num=2, figsize=(8, 11))
+		plt.ion()
+		plt.clf()
+		print('done')
+		print('generating subplots:')
+		def subhistplots(num, xvals, yvals, xstr, ystr):
+			print('\tcreating subplot ' + str(num) + '...', end="")
+			plt.subplot(2, 1, num)
+			plt.bar(xvals, yvals, width=--1, color='white', linewidth=1, 
+				edgecolor='red', hatch='////', align='edge', 
+				tick_label=np.around(v.realbins[1:],5))
+			plt.xlabel(ystr)
+			plt.ylabel(xstr)
+			plt.xticks(rotation=90)
+			print('done')
+			return
+		subhistplots(1, v.binlabels, v.counts, 'Counts', v.extstr)
+		subhistplots(2, v.binlabels, v.volbinsums, 'Volume', v.extstr)
+		plt.suptitle(strs[ext_col_idx])
+		plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+		plt.annotate(v.navgstr, xy=(0.55, 0.9), xytext=(0.55, 0.9), 
+						 textcoords='axes fraction')
+		plt.annotate(v.vavgstr, xy=(0.55, 0.8), xytext=(0.55, 0.8),
+						 textcoords='axes fraction') 
+
+		print('saving page' + '...', end="")
+		pdf.savefig()
+		print('done')
+		print('closing active page' + '...', end="")
+		plt.close()
+		print('done')
+
+		#if r = 
+		#Metadata outputs go here
+		
 
 
 
